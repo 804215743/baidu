cce86bd2f90d8e5d70092f159fe6668ea98c104e
804215743 <804215743@qq.com>
2019-12-11 01:15:49 -0800

    week14

---
+++ week14/code/my.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/param.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <pthread.h>
+#define NUM 4
+struct sendval{
+	int n;
+	int s;
+};
